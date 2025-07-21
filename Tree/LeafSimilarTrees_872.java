df1eb62127f6d0f257c12e58fb92b92c6ce32b41
Anupam Singh <eranupamsingh1008@gmail.com>
2025-07-21 16:06:25 +0530

    LeafSimilarTrees_872.java file added

---
+++ Tree/LeafSimilarTrees_872.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    private void dfs(TreeNode root, List<Integer> leaf){
+
+        if(root == null)
+            return;
+
+        if(root.left == null && root.right == null){
+            leaf.add(root.val);
+            return;
+        }
+
+        dfs(root.left, leaf);
+        dfs(root.right, leaf);
+    }
+    
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        
+        List<Integer> leaf1 = new ArrayList<>();
+        List<Integer> leaf2 = new ArrayList<>();
+
+        dfs(root1, leaf1);
+        dfs(root2, leaf2);
+
+        return leaf1.equals(leaf2);
+    }
+}
