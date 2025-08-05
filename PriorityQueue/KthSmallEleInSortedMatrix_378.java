460b2705a4ac5fee19b948d182a5a3c059deb5df
Anupam Singh <eranupamsingh1008@gmail.com>
2025-08-05 17:41:13 +0530

    KthSmallEleInSortedMatrix_378.java file added

---
+++ PriorityQueue/KthSmallEleInSortedMatrix_378.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+                (a,b)-> Integer.compare(b,a)
+        );
+        
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+
+                pq.add(matrix[i][j]);
+
+                if(pq.size() > k)
+                    pq.poll();
+            }
+        }
+
+        return pq.peek();
+    }
+}
