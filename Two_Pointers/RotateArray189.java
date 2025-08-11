a3e6d61486785dc7753354725c1b4af5a912b825
Anupam Singh <eranupamsingh1008@gmail.com>
2025-08-11 15:44:14 +0530

    RotateArray189.java file added

---
+++ Two_Pointers/RotateArray189.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+
+        int n = nums.length;
+        k %= n;
+
+        reverse(nums, 0, n - k - 1);
+        reverse(nums, n - k, n - 1);
+        reverse(nums, 0, n - 1);
+    }
+
+    private void reverse(int[] nums, int l, int r) {
+
+        while (l < r) {
+            swap(nums, l, r);
+            l++;
+            r--;
+        }
+    }
+
+    private void swap(int[] nums, int l, int r) {
+        int temp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = temp;
+    }
+}
