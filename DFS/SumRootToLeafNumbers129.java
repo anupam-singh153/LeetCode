b43a4369eb0a4de9f2503b7b19cf623cdc84fc26
Anupam Singh <eranupamsingh1008@gmail.com>
2025-08-21 19:33:23 +0530

    SumRootToLeafNumbers129.java file added

---
+++ DFS/SumRootToLeafNumbers129.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+
+    public int sumNumbers(TreeNode root) {
+
+        calculate(root, 0);
+        return sum;
+    }
+
+    public void calculate(TreeNode root, int temp) {
+
+        if (root == null)
+            return;
+
+        temp = temp * 10 + root.val;
+
+        if (root.left == null && root.right == null) {
+            sum += temp;
+            return;
+        }
+
+        calculate(root.left, temp);
+        calculate(root.right, temp);
+    }
+}
